3f52bccc505a6c29ac68a294e1fcb4b9d94debeb
ok5474565 <13725981731@163.com>
2024-06-19 22:14:58 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -5,12 +5,8 @@ def main():
     # 请确保视频文件路径是正确的
     video_file_path = "279233.mp4"
     
-    # 确保 width 和 height 是整数类型
-    width = 600
-    height = 400
-    
-    # 使用 st.video 嵌入视频，并设置宽度和高度
-    st.video(video_file_path, width=width, height=height)
+    # 使用 st.video 嵌入视频
+    st.video(video_file_path, width=600, height=400)
 
 if __name__ == "__main__":
     main()
