dad7de23446bad7d80336dcf511eda04a0ca84da
ok5474565 <13725981731@163.com>
2024-06-19 22:07:45 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -4,7 +4,7 @@ def main():
     st.title("视频播放示例")
     # 假设您的视频文件位于应用程序的相同目录中
     video_file = open("279233.mp4", "rb")
-    st.video(video_file)
+    st.video(video_file,width=400,height=600)
 
 if __name__ == "__main__":
     main()
