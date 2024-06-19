152ca476dc4739c59e79dac81470b82ddd3fa336
ok5474565 <13725981731@163.com>
2024-06-19 22:12:47 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -3,7 +3,7 @@ import streamlit as st
 def main():
     st.title("视频播放示例")
     # 假设您的视频文件位于应用程序的相同目录中
-    video_file = open("path_to_your_video.mp4", "rb")
+    video_file = open("279233.mp4", "rb")
     st.video(video_file)
 
 if __name__ == "__main__":
