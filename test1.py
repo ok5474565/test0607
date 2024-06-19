b538aa97d393673b3a162a073bd1f907adc5062c
ok5474565 <13725981731@163.com>
2024-06-19 22:11:26 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -2,11 +2,10 @@ import streamlit as st
 
 def main():
     st.title("视频播放示例")
-
-    # 假设您的视频文件位于应用程序的相同目录中
+    # 请确保视频文件路径是正确的
     video_file_path = "279233.mp4"
     
-    # 使用视频文件路径而不是文件对象
+    # 使用 st.video 嵌入视频
     st.video(video_file_path, width=600, height=400)
 
 if __name__ == "__main__":
