07e5b1ef3a9baabab4f8e4aa384c6ae58e53bc21
ok5474565 <13725981731@163.com>
2024-06-19 22:10:58 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -4,7 +4,7 @@ def main():
     st.title("视频播放示例")
 
     # 假设您的视频文件位于应用程序的相同目录中
-    video_file_path = "277293.mp4"
+    video_file_path = "279233.mp4"
     
     # 使用视频文件路径而不是文件对象
     st.video(video_file_path, width=600, height=400)
