1cced43b8a0219b5a17555a7352fcf3afc440fc7
ok5474565 <13725981731@163.com>
2024-06-19 21:58:11 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -3,7 +3,7 @@ import streamlit as st
 def main():
     st.title("在线视频播放示例")
     # 这是YouTube视频的URL示例
-    youtube_video = "https://www.bilibili.com/video/BV1Ez421U7gi/?spm_id_from=333.1007.tianma.1-2-2.click&vd_source=a270110c75c3c0d495aff16daf9b25cf"
+    youtube_video = "https://player.bilibili.com/player.html?aid=413672301&bvid=BV1MV41167e9&cid=208508865&page=1&t=60&autoplay=true"
     st.video(youtube_video)
 
 if __name__ == "__main__":
