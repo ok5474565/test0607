1b4955c70151a67aeb653123e18d703c8f983a35
ok5474565 <13725981731@163.com>
2024-06-19 21:58:47 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
 def main():
-    st.title("在线视频播放示例")
-    # 这是YouTube视频的URL示例
-    youtube_video = "https://player.bilibili.com/player.html?aid=413672301&bvid=BV1MV41167e9&cid=208508865&page=1&t=60&autoplay=true"
-    st.video(youtube_video)
+    st.title("视频播放示例")
+    # 假设您的视频文件位于应用程序的相同目录中
+    video_file = open("path_to_your_video.mp4", "rb")
+    st.video(video_file)
 
 if __name__ == "__main__":
     main()
