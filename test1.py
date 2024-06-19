97535c5d526ca3f535dae3aa39b5dfa71ce94dae
ok5474565 <13725981731@163.com>
2024-06-19 22:22:07 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -2,12 +2,11 @@ import streamlit as st
 
 def main():
     st.title("视频播放示例")
+    # 请确保视频文件路径是正确的
     video_file_path = "279233.mp4"
     
-    # 创建一个列，并将视频放入其中
-    col1 = st.columns([1, 2])  # 这里创建了一个两列布局，视频放在第二列
-    with col1[1]:  # 选择第二列
-        st.video(video_file_path)  # 视频会自动适应列的宽度
+    # 使用 st.video 嵌入视频
+    st.video(video_file_path)
 
 if __name__ == "__main__":
     main()
