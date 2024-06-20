d9f064a803f01137c093e698b77a350e08198586
ok5474565 <13725981731@163.com>
2024-06-20 20:21:08 +0800

    Update end11111.py

--- end11111.py
+++ end11111.py
@@ -6,13 +6,13 @@ from ciyuntu import main as ciyuntu_main
 st.sidebar.title("功能选择")
 choice = st.sidebar.selectbox(
     "选择一个功能:",
-    ("文本分词与高频词统计", "文本分词、高频词统计与词云图生成")
+    ("高频词统计条形图", "词云图生成")
 )
 
-if choice == "文本分词与高频词统计":
+if choice == "高频词统计条形图":
     # 调用 count.py 文件的 main 函数
     count_main()
-elif choice == "文本分词、高频词统计与词云图生成":
+elif choice == "词云图生成":
     # 调用 ciyuntu.py 文件的 main 函数
     ciyuntu_main()
 else:
